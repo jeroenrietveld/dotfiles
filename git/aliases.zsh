2870865198feb0c77e1369aa02c2f1ec51f691a6
jeroenrietveld <jeroen_rietveld13@msn.com>
2014-06-10 13:35:55 +0200

    added git pull --rebase alias (thanks Korstiaan :D)

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -18,3 +18,4 @@ alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gst='git status'
+alias gpr='git pull --rebase'
