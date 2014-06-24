63152585c87a4939823ec5c5693500f46c99a397
jeroenrietveld <jeroen_rietveld13@msn.com>
2014-06-24 15:13:22 +0200

    added projectile support for speedbar

--- emacs/personal/sr-speedbar.el
+++ emacs/personal/sr-speedbar.el
@@ -71,3 +71,48 @@
 
 ;; Highlight the current line
 (add-hook 'speedbar-mode-hook (lambda () (setq truncate-lines t)))
+
+;; Speedbar and projectile joining forces
+(defun find-current-project-root()
+  (setq current-dir (file-truename buffer-file-name))
+  (while (not (file-exists-p (concat current-dir ".git")))
+    (setq current-dir (file-name-directory (substring current-dir 0 -1))))
+  (concat current-dir ""))
+
+(defun speedbar-refresh-with-project (root-dir)
+  (when (and (not (equal root-dir sr-speedbar-last-refresh-dictionary))
+             (not (sr-speedbar-window-p)))
+    (setq sr-speedbar-last-refresh-dictionary root-dir))
+  (setq default-directory root-dir)
+  (speedbar-refresh))
+
+(defun open-current-project-speedbar (root-dir)
+  (if (not (sr-speedbar-exist-p))
+    (sr-speedbar-toggle))
+  (speedbar-refresh-with-project root-dir))
+
+(defun speedbar-expand-line-list (&optional arg)
+  (when arg
+    (message arg)
+    (re-search-forward (concat " " (car arg) "$"))
+    (speedbar-expand-line (car arg))
+    (speedbar-next 1)
+    (speedbar-expand-line-list (cdr arg))))
+
+(defun speedbar-open-current-buffer()
+  (interactive)
+  (let* ((root-dir (find-current-project-root))
+         (original-buffer-file-directory (file-name-directory (buffer-file-name)))
+         (relative-buffer-path (car (cdr (split-string original-buffer-file-directory root-dir))))
+         (parents (butlast (split-string original-buffer-file-directory root-dir)))
+         (original-window (get-buffer-window)))
+    (save-excursion
+      (open-current-project-speedbar root-dir)
+      (select-window (get-buffer-window speedbar-buffer))
+      (beginning-of-buffer)
+      (speedbar-expand-line-list parents)
+      (if (not (eq original-window (get-buffer-window speedbar-buffer)))
+          (select-window original-window)
+        (other-window 1)))))
+
+;;; sr-speedbar.el ends here
